1764213c4cbaaf380a826678e7399e7f8b0d6603
sksamuel <sam@sksamuel.com>
2022-09-18 23:48:19 -0500

    Support tags on metrics

--- aedile-micrometer/src/main/kotlin/com/sksamuel/aedile/micrometer/AedileMetrics.kt
+++ aedile-micrometer/src/main/kotlin/com/sksamuel/aedile/micrometer/AedileMetrics.kt
@@ -2,12 +2,17 @@ package com.sksamuel.aedile.micrometer
 
 import com.sksamuel.aedile.core.Cache
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tag
 import io.micrometer.core.instrument.binder.MeterBinder
 import io.micrometer.core.instrument.binder.cache.CaffeineCacheMetrics
 
-class AedileMetrics<K, V>(private val cache: Cache<K, V>, private val cacheName: String) : MeterBinder {
+class AedileMetrics<K, V>(
+   private val cache: Cache<K, V>,
+   private val cacheName: String,
+   private val tags: Collection<Tag> = emptyList()
+) : MeterBinder {
 
    override fun bindTo(registry: MeterRegistry) {
-      CaffeineCacheMetrics(cache.underlying().synchronous(), cacheName, emptyList()).bindTo(registry)
+      CaffeineCacheMetrics(cache.underlying().synchronous(), cacheName, tags).bindTo(registry)
    }
 }
