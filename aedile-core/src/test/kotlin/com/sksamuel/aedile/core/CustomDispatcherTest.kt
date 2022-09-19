127d681bcb542453eef8f18e4aa7f4116b6e7c4c
sksamuel <sam@sksamuel.com>
2022-09-18 23:07:22 -0500

    Added test for custom dispatcher

---
+++ aedile-core/src/test/kotlin/com/sksamuel/aedile/core/CustomDispatcherTest.kt
@@ -0,0 +1,24 @@
+package com.sksamuel.aedile.core
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import kotlinx.coroutines.asCoroutineDispatcher
+import java.util.concurrent.Executors
+
+class CustomDispatcherTest : FunSpec() {
+   init {
+      test("cache should use custom dispatcher") {
+         val dispatcher = Executors.newCachedThreadPool { r ->
+            val t = Thread(r)
+            t.name = "mcthreadface"
+            t
+         }.asCoroutineDispatcher()
+         val cache = caffeineBuilder().withDispatcher(dispatcher).build<String, String>()
+         cache.getOrPut("foo") {
+            Thread.currentThread().name shouldContain "mcthreadface"
+            "bar"
+         } shouldBe "bar"
+      }
+   }
+}
