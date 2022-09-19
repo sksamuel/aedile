89859906bd28b5212cabb7740664ee8483b1f1be
sksamuel <sam@sksamuel.com>
2022-09-18 23:47:48 -0500

    Updated metrics key params

--- aedile-micrometer/src/main/kotlin/com/sksamuel/aedile/micrometer/AedileMetrics.kt
+++ aedile-micrometer/src/main/kotlin/com/sksamuel/aedile/micrometer/AedileMetrics.kt
@@ -8,7 +8,6 @@ import io.micrometer.core.instrument.binder.cache.CaffeineCacheMetrics
 class AedileMetrics<K, V>(private val cache: Cache<K, V>, private val cacheName: String) : MeterBinder {
 
    override fun bindTo(registry: MeterRegistry) {
-      val c: com.github.benmanes.caffeine.cache.Cache<K, V> = cache.underlying().synchronous()
-      CaffeineCacheMetrics<K, V, com.github.benmanes.caffeine.cache.Cache<K, V>>(c, cacheName, emptyList()).bindTo(registry)
+      CaffeineCacheMetrics(cache.underlying().synchronous(), cacheName, emptyList()).bindTo(registry)
    }
 }
