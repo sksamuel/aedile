dcd0edfb5a1fe7cb7a6bd99471f9d415cb6ee1c9
Sam <sam@sksamuel.com>
2024-12-06 08:12:41 -0600

    Updated listeners to support default scope (#34)
    * Updated listeners to support default scope
    
    * Updated listeners to support default scope

--- aedile-core/src/main/kotlin/com/sksamuel/aedile/core/config.kt
+++ aedile-core/src/main/kotlin/com/sksamuel/aedile/core/config.kt
@@ -2,7 +2,10 @@ package com.sksamuel.aedile.core
 
 import com.github.benmanes.caffeine.cache.Caffeine
 import com.github.benmanes.caffeine.cache.RemovalCause
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.future.asCompletableFuture
 import kotlinx.coroutines.launch
 import kotlin.time.Duration
@@ -25,9 +28,22 @@ fun <K, V> Caffeine<K, V>.scheduler(scheduler: Scheduler): Caffeine<K, V> {
 
 /**
  * Specifies a listener that is notified each time an entry is removed.
+ * The provided listener will operate on a default coroutine scope.
+ *
  * See full docs at [Caffeine.removalListener].
  */
-fun <K, V> Caffeine<K, V>.removalListener(
+fun <K, V> Caffeine<K, V>.withRemovalListener(
+   listener: suspend (K?, V?, RemovalCause) -> Unit,
+): Caffeine<K, V> {
+   val scope = createScope("Aedile-RemovalListener-Scope")
+   return withRemovalListener(scope, listener)
+}
+
+/**
+ * Specifies a listener that is notified each time an entry is removed.
+ * See full docs at [Caffeine.removalListener].
+ */
+fun <K, V> Caffeine<K, V>.withRemovalListener(
    scope: CoroutineScope,
    listener: suspend (K?, V?, RemovalCause) -> Unit,
 ): Caffeine<K, V> {
@@ -40,9 +56,23 @@ fun <K, V> Caffeine<K, V>.removalListener(
 
 /**
  * Specifies a listener that is notified each time an entry is evicted.
+ * The provided listener will operate on a default coroutine scope.
+ *
  * See full docs at [Caffeine.evictionListener].
  */
-fun <K, V> Caffeine<K, V>.evictionListener(
+fun <K, V> Caffeine<K, V>.withEvictionListener(
+   listener: suspend (K?, V?, RemovalCause) -> Unit,
+): Caffeine<K, V> {
+   val scope = createScope("Aedile-EvictionListener-Scope")
+   return withEvictionListener(scope, listener)
+}
+
+/**
+ * Specifies a listener that is notified each time an entry is evicted.
+ *
+ * See full docs at [Caffeine.evictionListener].
+ */
+fun <K, V> Caffeine<K, V>.withEvictionListener(
    scope: CoroutineScope,
    listener: suspend (K?, V?, RemovalCause) -> Unit,
 ): Caffeine<K, V> {
@@ -72,4 +102,8 @@ fun <K, V> Caffeine<K, V>.expireAfterAccess(duration: Duration): Caffeine<K, V> 
  */
 fun <K, V> Caffeine<K, V>.expireAfterWrite(duration: Duration): Caffeine<K, V> {
    return this.expireAfterWrite(duration.toJavaDuration())
+}
+
+private fun createScope(name: String): CoroutineScope {
+   return CoroutineScope(Dispatchers.IO + CoroutineName(name) + SupervisorJob())
 }
