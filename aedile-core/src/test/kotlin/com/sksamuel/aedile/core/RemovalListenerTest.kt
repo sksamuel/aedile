31ccee9961525c2cd98f7fa5c52f581fa0c158a7
sksamuel <sam@sksamuel.com>
2024-12-06 08:06:32 -0600

    Updated listeners to support default scope

--- aedile-core/src/test/kotlin/com/sksamuel/aedile/core/RemovalListenerTest.kt
+++ aedile-core/src/test/kotlin/com/sksamuel/aedile/core/RemovalListenerTest.kt
@@ -1,7 +1,8 @@
 package com.sksamuel.aedile.core
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import com.github.benmanes.caffeine.cache.RemovalCause
-import io.kotest.assertions.timing.eventually
+import io.kotest.assertions.nondeterministic.eventually
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.delay
@@ -10,33 +11,35 @@ import kotlin.time.Duration.Companion.seconds
 class RemovalListenerTest : FunSpec() {
    init {
       test("cache should support removal listeners") {
-         var cause: RemovalCause? = null
-         val cache = caffeineBuilder<String, String> {
-            maximumSize = 1
-            removalListener = { _, _, removalCause -> cause = removalCause }
-         }.build()
+         var c: RemovalCause? = null
+         val cache = Caffeine.newBuilder()
+            .maximumSize(1)
+            .withRemovalListener { _, _, cause -> c = cause }
+            .asCache<String, String>()
          repeat(2) { k ->
             cache.put("$k") { "bar" }
          }
          eventually(5.seconds) {
-            cause shouldBe RemovalCause.SIZE
+            c shouldBe RemovalCause.SIZE
          }
       }
 
       test("cache should support suspendable removal listeners") {
-         var cause: RemovalCause? = null
-         val cache = caffeineBuilder<String, String> {
-            maximumSize = 1
-            removalListener = { _, _, removalCause ->
+         var c: RemovalCause? = null
+         val cache = Caffeine.newBuilder()
+            .maximumSize(1)
+            .withRemovalListener { _, _, cause ->
                delay(1)
-               cause = removalCause
+               c = cause
             }
-         }.build()
+            .asCache<String, String>()
+
          repeat(2) { k ->
             cache.put("$k") { "bar" }
          }
+
          eventually(5.seconds) {
-            cause shouldBe RemovalCause.SIZE
+            c shouldBe RemovalCause.SIZE
          }
       }
    }
